1cab144bba69c57c40b551892343268355e38db7
MaksymilianBilski <biger9@o2.pl>
2022-10-12 16:35:28 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,14 +4,12 @@ import 'flatpickr/dist/flatpickr.min.css';
 //input / button
 const timeInput = document.getElementById('datetime-picker');
 const button = document.querySelector('[type="button"]');
-//
 
 //catcihng time fields
 const daysCounter = document.querySelector('.value[data-days]');
 const hoursCounter = document.querySelector('.value[data-hours]');
 const minutesCounter = document.querySelector('.value[data-minutes]');
 const secondsCounter = document.querySelector('.value[data-seconds]');
-//
 
 //time getters
 const date = new Date();
@@ -20,26 +18,27 @@ const monthDay = date.getDate();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 const seconds = date.getSeconds();
-//
 
+//flatpickr options object
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    if (button.hasAttribute('disabled')) {
+      return
+    }
     if (
       Math.round(selectedDates[0].getTime() / 2.99999986) <
       Math.round(new Date().getTime() / 3)
     ) {
-      button.setAttribute('disabled', '');
       alert('Please choose a date in the future');
-    } else {
-      button.removeAttribute('disabled');
     }
   },
 };
 
+//function convert
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -58,41 +57,56 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
 let counterObject = 0;
 const calendar = flatpickr(timeInput, options);
 
+// check and change the button
+let isButtonClicked;
+button.addEventListener('click', () => {
+  button.setAttribute('disabled', '');
+  isButtonClicked = true;
+});
+
+//timer function
 const countDown = () => {
-  calendar.config.onChange.push(function difference() {
-    setInterval(() => {
-      counterObject = convertMs(
-        calendar.selectedDates[0].getTime() - new Date().getTime()
-      );
-      secondsCounter.innerHTML = counterObject.seconds;
-      minutesCounter.innerHTML = counterObject.minutes;
-      hoursCounter.innerHTML = counterObject.hours;
-      daysCounter.innerHTML = counterObject.days;
-      function addLeadingZero(value) {
-        if (
-          counterObject.seconds <= 9 ||
-          counterObject.minutes <= 9 ||
-          counterObject.hours <= 9 ||
-          counterObject.days <= 9
-        ) {
-          secondsCounter.innerHTML = secondsCounter.textContent.padStart(
-            2,
-            '0'
-          );
-          minutesCounter.innerHTML = minutesCounter.textContent.padStart(
-            2,
-            '0'
-          );
-          hoursCounter.innerHTML = hoursCounter.textContent.padStart(2, '0');
-          daysCounter.innerHTML = daysCounter.textContent.padStart(2, '0');
+  console.log('testlolejny');
+  if (isButtonClicked === true) {
+    //converting the difference in MS to normal date
+
+    calendar.config.onChange.push(function difference() {
+      setInterval(() => {
+        counterObject = convertMs(
+          calendar.selectedDates[0].getTime() - new Date().getTime()
+        );
+        secondsCounter.innerHTML = counterObject.seconds;
+        minutesCounter.innerHTML = counterObject.minutes;
+        hoursCounter.innerHTML = counterObject.hours;
+        daysCounter.innerHTML = counterObject.days;
+        //add zero in timers
+        function addLeadingZero(value) {
+          if (
+            counterObject.seconds <= 9 ||
+            counterObject.minutes <= 9 ||
+            counterObject.hours <= 9 ||
+            counterObject.days <= 9
+          ) {
+            secondsCounter.innerHTML = secondsCounter.textContent.padStart(
+              2,
+              '0'
+            );
+            minutesCounter.innerHTML = minutesCounter.textContent.padStart(
+              2,
+              '0'
+            );
+            hoursCounter.innerHTML = hoursCounter.textContent.padStart(2, '0');
+            daysCounter.innerHTML = daysCounter.textContent.padStart(2, '0');
+          }
         }
-      }
-      addLeadingZero();
-    }, 1000);
-  });
+        addLeadingZero();
+      }, 1000);
+    });
+  }
 };
 
 button.addEventListener('click', countDown);
