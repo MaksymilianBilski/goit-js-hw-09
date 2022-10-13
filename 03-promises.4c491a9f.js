196cc70e9782245cfa617a54cd127a6743e99634
MaksymilianBilski <103935400+MaksymilianBilski@users.noreply.github.com>
2022-10-13 02:47:14 +0000

    Deploying to gh-pages from @ MaksymilianBilski/goit-js-hw-09@a18f707098ef4fe10b4ced55a54d4369c36ff6c2 🚀

---
+++ 03-promises.4c491a9f.js
@@ -0,0 +1,2 @@
+document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]"),document.querySelector("button[type=submit]");new Promise((resolve,reject),(()=>{setTimeout((()=>{Math.random()}),1e3)}));
+//# sourceMappingURL=03-promises.4c491a9f.js.map
---
+++ 03-promises.779cd874.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]"),document.querySelector("button[type=submit]");new Promise((resolve,reject),(function(){setTimeout((function(){Math.random()}),1e3)}))}();
+//# sourceMappingURL=03-promises.779cd874.js.map
