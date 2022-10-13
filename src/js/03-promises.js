a18f707098ef4fe10b4ced55a54d4369c36ff6c2
MaksymilianBilski <biger9@o2.pl>
2022-10-13 04:46:47 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+const delayInput = document.querySelector('input[name=delay]');
+const stepInput = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+const buttonCreate = document.querySelector('button[type=submit]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +11,8 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+const promise = new Promise((resolve, reject), () => {
+  setTimeout(() => {
+    createPromise();
+  }, 1000);
+});
