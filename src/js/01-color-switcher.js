b76ff2bafa0aedff635bc0fe3db18651b4b02ac4
MaksymilianBilski <biger9@o2.pl>
2022-11-06 19:17:54 +0100

    zrobienie 2 metody w 1 zadaniu/ zaczecie drugiej metody w 2 zadaniu

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,52 @@
-const btnStart = document.querySelector('[type="button"][data-start]');
-const btnStop = document.querySelector('[type="button"][data-stop]');
-const body = document.querySelector('body');
-let timerId = null;
+// const btnStart = document.querySelector('[type="button"][data-start]');
+// const btnStop = document.querySelector('[type="button"][data-stop]');
+// const body = document.querySelector('body');
+// let timerId = null;
 
-btnStart.style.cursor = 'cell';
-btnStop.style.cursor = 'cell';
+// btnStart.style.cursor = 'cell';
+// btnStop.style.cursor = 'cell';
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const startColor = evt => {
+//   evt.target.setAttribute('disabled', '');
+//   timerId = setInterval(() => {
+//     body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// };
+
+// const disableColor = () => {
+//   btnStart.removeAttribute('disabled', '');
+//   const stopColor = clearInterval(timerId);
+// };
+
+// btnStart.addEventListener('click', startColor);
+// btnStop.addEventListener('click', disableColor);
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let colorInterval;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const startColor = evt => {
-  evt.target.setAttribute('disabled', '');
-  timerId = setInterval(() => {
+function changeColor() {
+  colorInterval = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  btnStart.setAttribute('disabled', 'disabled');
+}
+
+const stopColor = () => {
+  if (btnStart.hasAttribute('disabled')) {
+    clearInterval(colorInterval);
+    btnStart.removeAttribute('disabled');
+  }
 };
 
-const disableColor = () => {
-  btnStart.removeAttribute('disabled', '');
-  const stopColor = clearInterval(timerId);
-};
-
-btnStart.addEventListener('click', startColor);
-btnStop.addEventListener('click', disableColor);
+btnStart.addEventListener('click', changeColor);
+btnStop.addEventListener('click', stopColor);
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,95 +1,116 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// //input / button
+// const timeInput = document.getElementById('datetime-picker');
+// const button = document.querySelector('[type="button"]');
+// button.disabled = true;
+
+// //catcihng time fields
+// const daysCounter = document.querySelector('.value[data-days]');
+// const hoursCounter = document.querySelector('.value[data-hours]');
+// const minutesCounter = document.querySelector('.value[data-minutes]');
+// const secondsCounter = document.querySelector('.value[data-seconds]');
+
+// //time getters
+// const date = new Date();
+// const time = date.getTime();
+// const monthDay = date.getDate();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+
+// //flatpickr options object
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     if (selectedDates[0].getTime() < new Date().getTime()) {
+//       button.disabled = true;
+//       Notify.failure('Please choose a date in the future');
+//       return;
+//     } else if (selectedDates[0].getTime() > new Date().getTime()) {
+//       button.disabled = false;
+//     }
+//   },
+// };
+
+// //function convert
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// let counterObject = 0;
+// const calendar = flatpickr(timeInput, options);
+
+// const countDown = () => {
+//   // converting the difference in MS to normal date
+//   setInterval(() => {
+//     counterObject = convertMs(
+//       calendar.selectedDates[0].getTime() - new Date().getTime()
+//     );
+//     if (new Date().getTime() > calendar.selectedDates[0].getTime()) {
+//       return;
+//     } else secondsCounter.innerHTML = counterObject.seconds;
+//     minutesCounter.innerHTML = counterObject.minutes;
+//     hoursCounter.innerHTML = counterObject.hours;
+//     daysCounter.innerHTML = counterObject.days;
+//     //add zero in timers
+//     function addLeadingZero(value) {
+//       if (
+//         counterObject.seconds <= 9 ||
+//         counterObject.minutes <= 9 ||
+//         counterObject.hours <= 9 ||
+//         counterObject.days <= 9
+//       ) {
+//         secondsCounter.innerHTML = secondsCounter.textContent.padStart(2, '0');
+//         minutesCounter.innerHTML = minutesCounter.textContent.padStart(2, '0');
+//         hoursCounter.innerHTML = hoursCounter.textContent.padStart(2, '0');
+//         daysCounter.innerHTML = daysCounter.textContent.padStart(2, '0');
+//       }
+//     }
+//     addLeadingZero();
+//   }, 1000);
+//   button.disabled = true;
+// };
+
+// button.addEventListener('click', countDown);
+
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-//input / button
-const timeInput = document.getElementById('datetime-picker');
-const button = document.querySelector('[type="button"]');
-button.disabled = true;
-
-//catcihng time fields
-const daysCounter = document.querySelector('.value[data-days]');
-const hoursCounter = document.querySelector('.value[data-hours]');
-const minutesCounter = document.querySelector('.value[data-minutes]');
-const secondsCounter = document.querySelector('.value[data-seconds]');
-
-//time getters
-const date = new Date();
-const time = date.getTime();
-const monthDay = date.getDate();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-
-//flatpickr options object
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() < new Date().getTime()) {
-      button.disabled = true;
-      Notify.failure('Please choose a date in the future');
-      return;
-    } else if (selectedDates[0].getTime() > new Date().getTime()) {
-      button.disabled = false;
-    }
+    console.log(selectedDates[0]);
   },
 };
 
-//function convert
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+const input = document.querySelector('#datetime-picker');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-let counterObject = 0;
-const calendar = flatpickr(timeInput, options);
-
-const countDown = () => {
-  // converting the difference in MS to normal date
-  setInterval(() => {
-    counterObject = convertMs(
-      calendar.selectedDates[0].getTime() - new Date().getTime()
-    );
-    if (new Date().getTime() > calendar.selectedDates[0].getTime()) {
-      return;
-    } else secondsCounter.innerHTML = counterObject.seconds;
-    minutesCounter.innerHTML = counterObject.minutes;
-    hoursCounter.innerHTML = counterObject.hours;
-    daysCounter.innerHTML = counterObject.days;
-    //add zero in timers
-    function addLeadingZero(value) {
-      if (
-        counterObject.seconds <= 9 ||
-        counterObject.minutes <= 9 ||
-        counterObject.hours <= 9 ||
-        counterObject.days <= 9
-      ) {
-        secondsCounter.innerHTML = secondsCounter.textContent.padStart(2, '0');
-        minutesCounter.innerHTML = minutesCounter.textContent.padStart(2, '0');
-        hoursCounter.innerHTML = hoursCounter.textContent.padStart(2, '0');
-        daysCounter.innerHTML = daysCounter.textContent.padStart(2, '0');
-      }
-    }
-    addLeadingZero();
-  }, 1000);
-  button.disabled = true;
-};
-
-button.addEventListener('click', countDown);
+const fp = flatpickr(input, options);
