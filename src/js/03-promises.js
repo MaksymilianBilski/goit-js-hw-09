ae4fcc1da3c16aaccb3c6f2ff01fa66229a37eed
MaksymilianBilski <biger9@o2.pl>
2022-11-11 10:37:29 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -46,34 +46,31 @@ const stepInput = document.querySelector('input[name=step]');
 const amountInput = document.querySelector('input[name=amount]');
 const buttonCreate = document.querySelector('button[type=submit]');
 const form = document.querySelector('.form');
+let delay = delayInput.value;
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
-
-function addPromise(e) {
-  e.preventDefault();
-  let step = Number(stepInput.value);
-  let waitDelay = Number(delayInput.value);
-  for (let i = 0; i <= amountInput.value; i++) {
-    createPromise(i, waitDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    waitDelay += step;
+function createPromises(position, delay) {
+  const shouldResolve = Math.random() < 0.3;
+  if (shouldResolve) {
+    return({ position, delay });
+  }
+  else {
+    return({ position, delay });
   }
 }
 
-form.addEventListener('submit', addPromise);
+function addPromises(e) {
+  e.preventDefault();
+  new Promise = createPromises((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() < 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      else {
+        reject({ position, delay });
+      }
+    }, delay)
+  }).then(() => {
+
+  })
+}
